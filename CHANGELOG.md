e17914a0acf3edad51924059b2cdb0e60d48ae42
samuelreichor <samuelreichor@gmail.com>
2024-11-25 18:09:15 +0100

    I am Batman

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for craft-loanwords
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace samuelreichor\loanwords\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * craft-loanwords settings
+ */
+class Settings extends Model
+{
+}
